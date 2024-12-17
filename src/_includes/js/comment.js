1c102e8b10da6fb7e8eb604fed3f7a0c4698be98
sira313 <si.ecenk@gmail.com>
2024-12-18 03:32:41 +0700

    update comment.js

--- src/_includes/js/comment.js
+++ src/_includes/js/comment.js
@@ -207,7 +207,7 @@ function reply_comment(btn) {
 	/** @type {HTMLFormElement} */
 	const reply_form = comment_form.cloneNode(true);
 	reply_form.id = form_id;
-	reply_form.classList?.add("pl-5", "border-l-4", "border-base-200");
+	reply_form.classList?.add("pl-4", "border-l-4", "border-base-200");
 	reply_form.elements["submit"].innerText = "Reply";
 	reply_form.onsubmit = send_comment;
 
@@ -257,7 +257,7 @@ async function load_replies(id) {
 	document.getElementById(container_id)?.remove();
 	const container = document.createElement("section");
 	container.id = container_id;
-	container.className = `flex flex-col gap-4 pl-6 border-l-4 border-base-200`;
+	container.className = `flex flex-col gap-4 pl-4 border-l-4 border-base-200`;
 	for (const comment of data) {
 		const comment_el = document.createElement("div");
 		comment_el.innerHTML = `
