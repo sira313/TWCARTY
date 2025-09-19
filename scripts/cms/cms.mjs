e21b86bc29f8372c734c9e9419ae0bdc500307c3
sira313 <me@apoxi.cam>
2025-09-19 20:52:44 +0700

    fix title

--- scripts/cms/cms.mjs
+++ scripts/cms/cms.mjs
@@ -55,7 +55,7 @@ function createHtmlShell(title, content) {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>${title} | Dashboard</title>
+      <title>${title} | Simple CMS</title>
       <link href="/cms-assets/style.css" rel="stylesheet" type="text/css" />
     </head>
     <body class="bg-base-100 min-h-screen">
